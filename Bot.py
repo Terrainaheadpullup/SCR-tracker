11511ed6ca309658e0eca227ab34c966fa48552a
Terrainaheadpullup <52794963+Terrainaheadpullup@users.noreply.github.com>
2020-08-05 22:49:15 +0100

    Commit on master

--- Bot.py
+++ Bot.py
@@ -1,44 +1,27 @@
 import json
 import time
+import csv
 from datetime import datetime
-import gspread
 import os
 from urllib.request import urlopen
-from oauth2client.service_account import ServiceAccountCredentials
-
-scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-
-creds = ServiceAccountCredentials.from_json_keyfile_name('SheetConnection.json', scope)
-
-client = gspread.authorize(creds)
-
-sheet2 = client.open('SCR Tracker 3').sheet1
-
-B = 2
 
 for i in range(10000000000):
 
-    with urlopen('https://games.roblox.com/v1/games?universeIds=300039023') as response:
+    with urlopen('https://api.wheretheiss.at/v1/satellites/25544') as response:
         source2 = response.read()
         data2 = json.loads(source2)
-
-    file = open(r'C:\Users\Flynn\Documents\Python\dump2.json', 'w', encoding='utf-8')
-    json.dump(data2, file, ensure_ascii=False, indent=2)
-    file.close()
+        print(data2)
 
     now = datetime.now()
     dt = now.strftime('%Y-%m-%d %H:%M:%S')
 
-    file = open(r'C:\Users\Flynn\Documents\Python\dump2.json', 'r', encoding='utf-8')
-    text2 = json.load(file)
-    file.close()
-    print(text2)
-
-    for objects in text2['data']:
-        Count2 = objects['playing']
-        sheet2.update_cell(B,2, Count2)
-    sheet2.update_cell(B,1, dt)
-    B=B+1
-    time.sleep(120)
-
+    item = data2['latitude']
+    item2 = data2['longitude']
+    item3 = data2['altitude']
+    item4 = data2['velocity']
+    f = open(r'C:\Users\Flynn\Documents\GitHub\SCR-tracker\SCR Tracker.csv', 'a', encoding='utf-8' ,newline='')
+    thewriter = csv.writer(f) 
+    thewriter.writerow([dt, item, item2, item3, item4])
+    time.sleep(1)
+close(f)
 
